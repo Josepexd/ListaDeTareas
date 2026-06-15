cd05d760862954dc0f022e6342d742429b486309
Jeremy <20250623@ricaldone.edu.sv>
2026-06-15 12:10:56 -0600

    a

---
+++ js/controllers/index.controller.js
@@ -0,0 +1,28 @@
+//Arreglo donde se iran guardando las tareas
+let tareas = [];
+
+//Referencias a los elementos del DOM
+//Apuntamos a los elementos del HTML para poder manipularlos desde JavaScript
+const txtTarea = document.getElementById("txtTarea");
+const prioridad = document.getElementById("prioridad");
+const btnAgregar = document.getElementById("btnAgregar");
+const listaTareas = document.getElementById("listaTareas");
+const contador = document.getElementById("contador");
+
+
+//Llamaremos a la funcion "agregarTarea" cada vez que se haga click en el boton "Agregar"
+btnAgregar.addEventListener("click", agregarTarea);
+function agregarTarea(){
+    const nombreTarea = txtTarea.value.trim();
+    const prioridadTarea = prioridad.value;
+}
+
+if(nombreTarea == "" || prioridadTarea == ""){
+    Swal.fire({
+            icon: "warning",
+            title: "Campos incompletos",
+            text: "Debes escribir una tarea y seleccionar una prioridad.",
+            confirmButtonText: 'Aceptar'
+        });
+        return;
+}
